0eef4f0dd749c6958d9e96a692e327415773b6d1
Ted Williams <williamsted1001@gmail.com>
2026-05-25 08:27:58 +0300

    Contact page modified

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -25,6 +25,25 @@ tabButtons.forEach(button => {
     })
 })
 
+//slideshow
+const slides = document.querySelectorAll('.slide')
+let currentSlide = 0
+function showSlide(index) {
+    slides.forEach(slide => {
+        slide.classList.remove('opacity-100')
+        slide.classList.add('opacity-0')
+    })
+    slides[index].classList.remove('opacity-0')
+    slides[index].classList.add('opacity-100')
+}
+setInterval(() => {
+    currentSlide++
+    if(currentSlide >= slides.length){
+        currentSlide = 0
+    }
+    showSlide(currentSlide)
+}, 4000);
+
 //fetch footer
 fetch('/src/components/footer.html')
     .then(res => res.text())
